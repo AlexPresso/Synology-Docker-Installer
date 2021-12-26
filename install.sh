24e51f965c9f954adf4ab30c8e740aaf3e749950
AlexPresso <blond.alexandre@hotmail.com>
2021-12-26 20:28:11 +0100

    Improvements

--- install.sh
+++ install.sh
@@ -22,21 +22,24 @@ function info {
 # DOCKER INSTALLATION
 ###############################
 
+info "NAS kernel: $kernel, NAS architecture: $arch"
+
+info "Preparing to install docker..."
 info "Fetching latest docker $arch version..."
 latest=$(curl --silent "$baseDownloadUrl/" | grep -oP 'href="docker-\K[0-9]+\.[0-9]+\.[0-9]' | sort -t. | tail -1)
 status=$?
 
-if [ $status -neq 0 ]; then
+if [ $status -ne 0 ]; then
 	info "No docker version found for your NAS architecture ($arch)."
 	exit 1
 fi
 
 filename="docker-$latest.tgz"
 info "Downloading $filename ..."
-wget "$baseDownloadUrl/$filename" > "/tmp/$filename"
+wget -q -O - "$baseDownloadUrl/$filename" > "/tmp/$filename"
 
-info "Unpacking..."
-tar xzvf "/tmp/$filename"
+info "Unpacking $filename ..."
+tar xzf "/tmp/$filename" -C /tmp
 cp "/tmp/docker/*" /usr/bin
 
 info "Creating docker directory outside DSM..."
@@ -58,23 +61,25 @@ echo '{
 info "Running docker daemon..."
 dockerd &
 
+info "Done installing docker $latest."
 
 ###############################
 # DOCKER-COMPOSE INSTALLATION
 ###############################
 
+info "Installing docker-compose..."
 info "Fetching latest docker-compose version..."
-latestCompose=$(curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+latestCompose=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 status=$?
 
-if [ $status -neq 0 ]; then
+if [ $status -ne 0 ]; then
 	info "Cannot find latest docker-compose version"
 	exit 1
 fi
 
 filename="docker-compose-$kernel-$arch"
-info "Downloading $filename ..."
-curl -L "https://github.com/docker/compose/releases/download/$latestCompose/$filename" -o /usr/local/bin/docker-compose
+info "Downloading $filename $latestCompose ..."
+curl --silent -L "https://github.com/docker/compose/releases/download/$latestCompose/$filename" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-
+info "Done installing docker-compose $latestCompose"
